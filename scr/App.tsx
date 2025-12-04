a449c5e3d8f3c1698ac159787536503d92784a50
ohhmoon85-ops <ohhmoon85@gmail.com>
2025-12-04 14:42:35 +0900

    Create App.tsx

---
+++ scr/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      ✅ React is working! <br />
+      Welcome to Choi Byung-hyuk's site.
+    </div>
+  );
+}
